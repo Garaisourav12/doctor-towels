60cbe426cca271739258cf35e1802d0bd2df6dae
Garaisourav12 <garaisourav12@gmail.com>
2024-05-02 20:28:59 +0530

    3rd  commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header>
-        <p className='bg-[#212b36] text-center text-sm text-white p-3 mt-5'>Receive a Free Cotton Bath Towel on orders above ₹1,699</p>
+        <p className='bg-[#212b36] text-center text-[.5rem] md:text-[.8rem] text-white p-3 mt-5'>Receive a Free Cotton Bath Towel on orders above ₹1,699</p>
     </header>
   )
 }
